7e4308af21af6c9bc42c842750130e29ee0342ba
WoojinDeve <jwj060111@gmail.com>
2022-12-12 19:49:39 +0900

    feat: operator enum type

--- oop-pratice/src/test/java/org/example/calculator/CalculatorTest.java
+++ oop-pratice/src/test/java/org/example/calculator/CalculatorTest.java
@@ -1,8 +1,7 @@
-package org.example;
+package org.example.calculator;
 
-import org.assertj.core.util.Streams;
+import org.example.calculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
