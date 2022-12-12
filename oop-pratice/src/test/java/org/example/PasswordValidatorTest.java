0a4c034812020e5fe0a2e21aa4590323c2da6163
WoojinDeve <jwj060111@gmail.com>
2022-12-12 15:10:11 +0900

    feat: random password test

--- oop-pratice/src/test/java/org/example/PasswordValidatorTest.java
+++ oop-pratice/src/test/java/org/example/PasswordValidatorTest.java
@@ -4,6 +4,11 @@ package org.example;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
+
+import static org.assertj.core.api.Assertions.*;
 
 /*
  * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
@@ -14,10 +19,17 @@ public class PasswordValidatorTest {
 
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
-    void validatePasswordTest(){
-        Assertions.assertThatCode(() -> PasswordValidator.validate("serverwizard"))
+    void validatePasswordTest() {
+        assertThatCode(() -> PasswordValidator.validate("serverwizard"))
                 .doesNotThrowAnyException();
     }
 
-
+    @DisplayName("비밀번호가 8자 미만 또는 12자를 초과하는 경우 IllgalArgumentException 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+    void validatePasswordTest_Exception(String password){
+        assertThatCode(() -> PasswordValidator.validate(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야합니다.");
+    }
 }
