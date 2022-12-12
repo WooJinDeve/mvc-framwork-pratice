3ace89e26439e06e8b9c1ba90749622a6a309313
WoojinDeve <jwj060111@gmail.com>
2022-12-12 21:23:35 +0900

    feat: customer impl

--- oop-pratice/src/test/java/org/example/restaurant/CustomerTest.java
+++ oop-pratice/src/test/java/org/example/restaurant/CustomerTest.java
@@ -1,6 +1,10 @@
 package org.example.restaurant;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 /*
 * 음식점에서 음식을 주문하는 과정 구현
@@ -23,8 +27,14 @@ import org.junit.jupiter.api.Test;
 public class CustomerTest {
 
 
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Cooking cooking = new Cooking();
 
+        Assertions.assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
