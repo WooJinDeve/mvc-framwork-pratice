f5695ea5580ccb5a844e862a2adb8806815efab6
WoojinDeve <jwj060111@gmail.com>
2022-12-12 15:16:45 +0900

    init : reupload

--- oop-pratice/src/main/java/org/example/User.java
+++ oop-pratice/src/main/java/org/example/User.java
@@ -3,9 +3,8 @@ package org.example;
 public class User {
     private String password;
 
-    public void initPassword(){
-        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
-        String randomPassword = randomPasswordGenerator.generatePassword();
+    public void initPassword(PasswordGenerator passwordGenerator){
+        String randomPassword = passwordGenerator.generatePassword();
 
         if (randomPassword.length() >= 8 && randomPassword.length() <= 12)
             this.password = randomPassword;
--- oop-pratice/src/test/java/org/example/UserTest.java
+++ oop-pratice/src/test/java/org/example/UserTest.java
@@ -15,10 +15,23 @@ class UserTest {
         User user = new User();
 
         //when
-        user.initPassword();
+        user.initPassword(new CorrectFixedPasswordGenerator());
 
         //then
         Assertions.assertThat(user.getPassword()).isNotNull();
+    }
+
+    @DisplayName("패스워드가 요구사항에 부합하지 않아 초기화가 되지 않는다.")
+    @Test
+    void passwordTest2(){
+        //given
+        User user = new User();
+
+        //when
+        user.initPassword(new WrongFixedPasswordGenerator());
+
+        //then
+        Assertions.assertThat(user.getPassword()).isNull();
     }
 
 }
