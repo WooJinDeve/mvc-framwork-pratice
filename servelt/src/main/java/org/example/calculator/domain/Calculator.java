218d2dd95e3eb756d18a382a58b7b611931ddbe2
WoojinDeve <jwj060111@gmail.com>
2022-12-13 12:10:33 +0900

    init: servlet calculator service

---
+++ servelt/src/main/java/org/example/calculator/domain/Calculator.java
@@ -0,0 +1,26 @@
+package org.example.calculator.domain;
+
+import org.example.calculator.tobe.*;
+
+import java.util.List;
+
+public class Calculator {
+
+    private static final List<ArithmeticOperator> arithmeticOperators = List.of(
+            new AdditionOperator(),
+            new SubtractionOperator(),
+            new MultiplicationOperator(),
+            new DivisionOperator());
+
+    public static int calculate(int operand1, String operator, int operand2) {
+        return EnumArithmeticOperator.calculator(operand1, operator, operand2);
+    }
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperators -> arithmeticOperators.supports(operator))
+                .map(arithmeticOperators -> arithmeticOperators.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+    }
+}
