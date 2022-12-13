ee988c830b4dfa847f4c5adddd63017db850dc00
WoojinDeve <jwj060111@gmail.com>
2022-12-13 10:44:40 +0900

    feat: was calculator step3

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -34,7 +36,8 @@ public class CustomWebApplicationServer {
                 * Step2 - 사용자 요청이 들어올때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
                 * */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                //new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
