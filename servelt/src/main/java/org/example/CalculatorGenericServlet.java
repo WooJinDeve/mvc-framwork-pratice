1d338884e067bbf98267158e53da828407ea7794
WoojinDeve <jwj060111@gmail.com>
2022-12-13 12:12:49 +0900

    feat: genericservlet calculator

--- servelt/src/main/java/org/example/CalculatorGenericServlet.java
+++ servelt/src/main/java/org/example/CalculatorGenericServlet.java
@@ -5,23 +5,19 @@ import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/calculate")
+@WebServlet("calculate")
 @Deprecated
-public class CalculatorServlet implements Servlet {
-
+public class CalculatorGenericServlet extends GenericServlet {
     private static final Logger logger = LoggerFactory.getLogger(CalculatorServlet.class);
-    private ServletConfig servletConfig;
 
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        logger.info("init");
-        this.servletConfig = servletConfig;
-    }
 
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
@@ -34,20 +30,5 @@ public class CalculatorServlet implements Servlet {
 
         PrintWriter writer = response.getWriter();
         writer.println(result);
-    }
-
-    @Override
-    public void destroy() {
-        // resource release
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return this.servletConfig;
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
     }
 }
